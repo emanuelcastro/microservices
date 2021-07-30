49e5477822a31da07f04062f87c3f702d01911b8
Emanuel Castro da Ponte <emanuel.ponte@serpro.gov.br>
2021-07-29 21:22:50 -0300

    docker compose

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +16,15 @@ public class CurrencyExchangeController {
 
 	@Autowired
 	private Environment environment;
+	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 //		CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, 
 //								BigDecimal.valueOf(50));
+		
+		logger.info("retrieveExchangeValue called with {} to {}", from, to);
 		
 		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 		
